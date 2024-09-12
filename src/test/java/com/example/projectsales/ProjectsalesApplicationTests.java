b33e2f3bb52abd19f05afa4991559ff8b8bfa562
Icaro Iyusuka <icaroiyusuka@gmail.com>
2024-09-12 16:22:21 -0300

    patch1

---
+++ src/test/java/com/example/projectsales/ProjectsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
